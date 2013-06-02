c7e2a0d11741551c16214a91b3d77aa00c8552d7
karthik050782 <karthikeyan.psy@gmail.com>
2013-06-02 15:06:44 +0530

    i9103 Changes

---
+++ phoneapps.mk
@@ -0,0 +1,53 @@
+local-phone-apps = $(private-phone-apps)
+
+private-phone-apps := ApplicationsProvider \
+					  BackupRestoreConfirmation \
+					  CertInstaller \
+					  DefaultContainerService \
+					  KeyChain \
+					  Stk \
+					  UserDictionaryProvider \
+					  VpnDialogs \
+					  WAPPushManager
+
+private-lockscreen-apps := PickUpTutorial \
+						   AccuweatherDaemon \
+						   AccuweatherWidget
+
+private-phone-apps2 :=  AllshareDMC \
+						AllshareMediaServer \
+						AllSharePlay \
+						BGCleaner \
+						BroadcomMEMSService \
+						CapabilityManagerService \
+						CertInstaller \
+						ChromeBookmarksSyncAdapter \
+						ContextAwareService \
+						DeepSea \
+						DirectShareManager \
+						DrmProvider \
+						DSMForwarding \
+						DSMLawmo \
+						EdmSysScopeService \
+						EdmVpnServices \
+						EnterprisePermissions \
+						Hearingdro \
+						INDI_Server \
+						JobManager \
+						NetworkLocation \
+						NoiseField \
+						OmaCP \
+						OmaDrmPopup \
+						PackageInstaller \
+						PCWClientS \
+						PhaseBeam \
+						PRUI \
+						SecurityProvider \
+						ShaoNv \
+						TwDVFSApp \
+						VpnDialogs \
+						WfdBroker \
+						BluetoothAvrcp \
+						BluetoothMap \
+						BluetoothTest
+#DreyeforSamsung
